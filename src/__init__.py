e4e152e8ee24ae0819a262da4e034b4c8ce5603c
Joao Campos <joao.campos@cloud-iq.com>
2018-12-06 21:06:17 +0000

    fixed import lint warning

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,8 @@
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
 from .shared import DB
 from .app_config import APP
 from .db.db_config import create_database
-
-from sqlalchemy.engine import Engine
-from sqlalchemy import event
 
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
